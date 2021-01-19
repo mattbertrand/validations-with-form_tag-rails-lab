89fe6a156ec4fe1ba1b6d423d45e68c08d7f0a6d
github username <github email address>
2021-01-19 10:16:37 -0800

    commit tree eec588df1b23b46856e0fffd85d6cface6c11849

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:edit, :update, :new, :create, :show]
+  resources :posts, only: [:edit, :update, :new, :create, :show]
 end
